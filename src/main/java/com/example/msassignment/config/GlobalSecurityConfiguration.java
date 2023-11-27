191adaa3d35db24ac50b0ba65569ad2006960728
Alejandro Flores <alejandro.flores.a@ucb.edu.bo>
2023-11-27 10:57:49 -0400

    security added

--- src/main/java/com/example/msassignment/config/GlobalSecurityConfiguration.java
+++ src/main/java/com/example/msassignment/config/GlobalSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.msassignment.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class GlobalSecurityConfiguration {
+    @Autowired
+    private SecurityProperties securityProperties;
     private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
 
     public GlobalSecurityConfiguration(TokenConverterProperties properties) {
@@ -29,20 +32,26 @@ public class GlobalSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeHttpRequests) -> {
-            authorizeHttpRequests
-                    .requestMatchers(HttpMethod.POST, "api/v1/assignment").hasRole("CREATE-COURSES")
-                    .requestMatchers(HttpMethod.PUT, "api/v1/assignment/{id}/user/{userId}").hasRole("VIEW-CONTENT")
-                    .requestMatchers(HttpMethod.POST, "api/v1/assignment/{assignmentId}").hasRole("VIEW-CONTENT")
-                    .requestMatchers(HttpMethod.GET, "api/v1/assignment/all/{courseId}").hasAnyRole("VIEW-CONTENT", "CREATE-COURSES")
-                    .requestMatchers(HttpMethod.GET, "api/v1/assignment-type").hasAnyRole("VIEW-CONTENT", "CREATE-COURSES")
+        http.authorizeHttpRequests(authz -> {
+            for (SecurityProperties.Route route : securityProperties.getRoutes()) {
+                HttpMethod httpMethod = getHttpMethod(route.getMethod());
+                if (httpMethod != null) {
+                    if (route.getRoles() == null || route.getRoles().isEmpty()) {
+                        authz.requestMatchers(httpMethod, route.getPath()).permitAll();
+                    } else {
+                        authz.requestMatchers(httpMethod, route.getPath())
+                                .hasAnyRole(route.getRoles().toArray(new String[0]));
+                    }
 
-
-
-                    .anyRequest().permitAll();
-                    //.requestMatchers("/**").permitAll();
-            //.anyRequest().permitAll();
+                    /*
+                    authz.requestMatchers(httpMethod, route.getPath())
+                            .hasAnyRole(route.getRoles().toArray(new String[0]));
+                */
+                }
+            }
+            authz.anyRequest().permitAll();
         });
+
         http.oauth2ResourceServer((oauth2) -> {
             oauth2.jwt((jwt) -> jwt.jwtAuthenticationConverter(keycloakJwtTokenConverter));
         });
@@ -50,6 +59,24 @@ public class GlobalSecurityConfiguration {
         http.cors(withDefaults());
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+    private HttpMethod getHttpMethod(String method) {
+        if (method == null) return null;
+        switch (method.toUpperCase()) {
+            case "GET":
+                return HttpMethod.GET;
+            case "POST":
+                return HttpMethod.POST;
+            case "PUT":
+                return HttpMethod.PUT;
+            case "DELETE":
+                return HttpMethod.DELETE;
+            case "PATCH":
+                return HttpMethod.PATCH;
+            // Agrega más casos si es necesario
+            default:
+                return null;
+        }
     }
 }
 
